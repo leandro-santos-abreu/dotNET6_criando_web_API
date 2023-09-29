1087402b80af839ae81a911477705d5e99a82a6c
Leandro Abreu <leandro.sabreu@uvvnet.com.bru>
2023-09-28 23:32:08 -0300

    Finalizando Curso ".NET 6 e Identity: implementando controle de usuário"

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController: ControllerBase
+    {
+        private UsuarioService usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            this.usuarioService = usuarioService;
+        }
+
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> CadastraUsuarioAsync(CreateUsuarioDto dto)
+        {
+            await usuarioService.CadastraAsync(dto);
+            return Ok("Usuário Cadastrado!");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginUsuarioDto dto)
+        {
+            var token = await usuarioService.LoginAsync(dto);
+            return Ok(token);
+        }
+    }
+}
---
+++ UsuariosApi/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosApi.Models
+{
+    public class Usuario: IdentityUser
+    {
+        public Usuario() : base() { }
+        public DateTime DataNascimento { get; set; }
+    }
+}
